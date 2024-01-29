074f5d119da6ae85107c8a26d94dc8d8a471b8ec
Kang Shen <k.shen02@outlook.com>
2024-01-29 21:52:40 +0000

    Web App First Commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import NavigationBar from './components/NavigationBar.js'
+import IndexMain from './components/IndexMain.js';
+import HomeMain from './components/HomeMain.js';
+import EpisodesMain from './components/EpisodesMain.js';
+import SignIn from './components/SignIn.js';
+
+export function Login() {
+  return (
+    <div className="Login">
+      <NavigationBar />
+      <SignIn />
+    </div>
+  )
+}
+
+export function Home() {
+  return (
+    <div className="Home">
+      <NavigationBar />
+      <HomeMain />
+    </div>
+  )
+}
+
+export function Episodes() {
+  return (
+    <div className="Episodes">
+      <NavigationBar />
+      <EpisodesMain />
+    </div>
+  )
+}
+
+export function Create() {
+  return (
+    <div className="Create">
+      <NavigationBar />
+      <h1>Create</h1>
+    </div>
+  )
+}
+
+export function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <IndexMain />
+    </div>
+  );
+}
