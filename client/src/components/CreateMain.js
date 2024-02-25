c6f15f38cb19e99a5f2fac7acd03821af261d777
Kang Shen <k.shen02@outlook.com>
2024-02-25 20:41:26 +0000

    Implemented form submission to backend on "Create" page

--- client/src/components/CreateMain.js
+++ client/src/components/CreateMain.js
@@ -3,26 +3,44 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Copyright from './Copyright.js'
-import { Button, Divider } from '@mui/material';
-import AudioPlayerSlider from './AudioPlayerSlider.js'
+import Copyright from './Copyright.js';
+import { Button, Collapse, Divider } from '@mui/material';
 
-// import axios from 'axios';
+import axios from 'axios';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default class CreateMain extends React.Component {
-    state = {
-        // episodes: []
+
+    constructor(props) {
+        super(props);
+        this._handleTextFieldChange = this._handleTextFieldChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
 
-    componentDidMount() {
-        // axios.get(`http://localhost:3001/episodes`)
-        //     .then(res => {
-        //         const episodes = res.data;
-        //         this.setState({ episodes });
-        //     })
+    state = {
+        topicKeywords: '',
+        responseData: '',
+        show: false,
+    }
+
+    _handleTextFieldChange(e) {
+        const topicKeywords = e.target.value
+        this.setState({ topicKeywords });
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+        // alert('Form submitted: ' + this.state.topicKeywords)
+
+        axios.post('http://localhost:3001/episodes/generate/', {
+            topicKeywords: this.state.topicKeywords
+        })
+            .then((response) => {
+                this.setState({ responseData: response.data, show: true });
+                // window.location.href = "http://localhost:3000/create-generated?topic=" + this.state.topicKeywords + "&res=" + response.data;
+            });
     }
 
     render() {
@@ -31,47 +49,81 @@ export default class CreateMain extends React.Component {
             <ThemeProvider theme={defaultTheme}>
                 <CssBaseline />
                 <main>
-                    <TextField
-                        id="filled-multiline-static"
-                        label="Topic Keywords"
-                        multiline
-                        rows={4}
-                        defaultValue=""
-                        variant="filled"
-                        sx={{
-                            width: "40vw",
-                            maxWidth: '100%',
+                    <form onSubmit={this._handleSubmit}>
+                        <TextField
+                            id="filled-multiline-static"
+                            label="Topic Keywords"
+                            multiline
+                            rows={4}
+                            defaultValue=""
+                            variant="filled"
+                            sx={{
+                                width: "40vw",
+                                maxWidth: '100%',
+                                marginTop: "50px",
+                                marginLeft: "30vw"
+                            }}
+                            onChange={this._handleTextFieldChange}
+                        />
+                        <Button
+                            variant="contained"
+                            sx={{
+                                width: "10vw",
+                                minWidth: "100px",
+                                maxWidth: "150px",
+                                marginTop: "50px",
+                                marginLeft: "45vw"
+                            }}
+                            type='submit'
+                        >
+                            Generate
+                        </Button>
+                    </form>
+                    <Collapse in={this.state.show}>
+                        <Divider sx={{
+                            width: "80vw",
                             marginTop: "50px",
-                            marginLeft: "30vw"
-                        }}
-                    />
-                    <Button
-                        variant="contained"
-                        sx={{
-                            width: "10vw",
-                            minWidth: "100px",
-                            marginTop: "50px",
-                            marginLeft: "45vw"
-                        }}
-                    >
-                        Generate
-                    </Button>
-                    <Divider sx={{
-                        width: "80vw",
-                        marginTop: "50px",
-                        marginLeft: "10vw",
-                        fontSize: "18px"
-                    }}>
-                        Output
-                    </Divider>
-                    <AudioPlayerSlider />
+                            marginLeft: "10vw",
+                            fontSize: "18px"
+                        }}>
+                            Output
+                        </Divider>
+                        <TextField
+                            id="filled-multiline-static"
+                            multiline
+                            rows={4}
+                            defaultValue={this.state.responseData}
+                            variant="filled"
+                            sx={{
+                                width: "40vw",
+                                maxWidth: '100%',
+                                marginTop: "50px",
+                                marginLeft: "30vw"
+                            }}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                        <Button
+                            variant="contained"
+                            sx={{
+                                width: "10vw",
+                                minWidth: "100px",
+                                maxWidth: "150px",
+                                marginTop: "50px",
+                                marginLeft: "45vw"
+                            }}
+                        >
+                            Upload
+                        </Button>
+                    </Collapse>
                 </main>
                 {/* Footer */}
                 <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
                     <Copyright />
                 </Box>
                 {/* End footer */}
-            </ThemeProvider>
+            </ThemeProvider >
         );
     }
 }
---
+++ client/src/components/EpisodesMain.js
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Box, Typography, Container, TextField, Divider } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Copyright from './Copyright.js'
+
+import axios from 'axios';
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default class EpisodesMain extends React.Component {
+    state = {
+        episodes: []
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:3001/episodes`)
+            .then(res => {
+                const episodes = res.data;
+                this.setState({ episodes });
+            })
+    }
+
+    render() {
+        return (
+
+            <ThemeProvider theme={defaultTheme}>
+                <CssBaseline />
+                <main>
+                    <Box
+                        sx={{
+                            width: "60vw",
+                            maxWidth: '100%',
+                            marginTop: "50px",
+                            marginLeft: "20vw"
+                        }}
+                    >
+                        <TextField fullWidth label="Search episodes..." id="search" />
+                    </Box>
+                    <Divider sx={{
+                        width: "80vw",
+                        marginTop: "50px",
+                        marginLeft: "10vw",
+                        fontSize: "18px",
+                        fontWeight: "bold"
+                    }}>
+                        All Episodes
+                    </Divider>
+                    <Container sx={{ py: 8 }} maxWidth="md">
+                        <Grid container spacing={4}>
+                            {this.state.episodes.map((episode) => (
+                                <Grid item key={episode} xs={12} sm={6} md={4}>
+                                    <Card
+                                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <CardMedia
+                                            component="div"
+                                            sx={{
+                                                // 16:9
+                                                pt: '56.25%',
+                                            }}
+                                            image="https://source.unsplash.com/random?wallpapers"
+                                        />
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {episode.title}
+                                            </Typography>
+                                            <Typography>
+                                                {episode.description}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">View</Button>
+                                            <Button size="small">Edit</Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </main>
+                {/* Footer */}
+                <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+                    <Copyright />
+                </Box>
+                {/* End footer */}
+            </ThemeProvider>
+        );
+    }
+}
